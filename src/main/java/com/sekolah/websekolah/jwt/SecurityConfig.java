d8bcc6899bfe4960457e171802259e98eaeb9ed0
yogiset <the3verlastin9@gmail.com>
2023-11-20 20:46:53 +0700

    add @EnableMethodSecurity for auth role

--- src/main/java/com/sekolah/websekolah/jwt/SecurityConfig.java
+++ src/main/java/com/sekolah/websekolah/jwt/SecurityConfig.java
@@ -3,6 +3,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +18,7 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
 
