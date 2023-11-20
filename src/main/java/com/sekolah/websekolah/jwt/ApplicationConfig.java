249bcf98c0cdf0731ca47faa4de3880a60a91eee
yogiset <the3verlastin9@gmail.com>
2023-11-20 20:45:28 +0700

    added User and admin account for userdetailsservice

--- src/main/java/com/sekolah/websekolah/jwt/ApplicationConfig.java
+++ src/main/java/com/sekolah/websekolah/jwt/ApplicationConfig.java
@@ -11,26 +11,36 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@RequiredArgsConstructor
+@EnableMethodSecurity
 public class ApplicationConfig {
-
-    private final UserRepository userRepository;
-
     @Bean
     public UserDetailsService userDetailsService(){
-        return username -> {
-            User user = userRepository.findByEmail(username)
-                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-            return new CustomUserDetails(user); // Wrap the User entity in CustomUserDetails
-        };
+//        UserDetails admin = User.withUsername("yogi@gmail.com")
+//                .password(passwordEncoder().encode("yogiset"))
+//                .roles("USER")
+////                .accountExpired(false)
+////                .accountLocked(false)
+////                .credentialsExpired(false)
+////                .disabled(false)
+//                .build();
+//        UserDetails user = User.withUsername("john@gmail.com")
+//                .password(passwordEncoder().encode("johnna"))
+//                .roles("USER","ADMIN","HR")
+////                .accountExpired(false)
+////                .accountLocked(false)
+////                .credentialsExpired(false)
+////                .disabled(false)
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user); //get user,password from UserDetailsService
+        return new CustomUserDetailsService(); // get user,password,email,status,etc from database
     }
-
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -41,7 +51,7 @@ public class ApplicationConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-      return config.getAuthenticationManager();
+        return config.getAuthenticationManager();
     }
 
     @Bean
